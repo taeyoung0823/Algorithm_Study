ad290ee925b895ae7a108f8fd99ebcd2d8869e8e
taeyoung0823 <154206361+taeyoung0823@users.noreply.github.com>
2025-09-06 15:46:41 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 9.09 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -0,0 +1,10 @@
+def solution(array, commands):
+    answer = []
+    for command in commands:
+        i,j,k=command
+        sliced = array[i-1:j]
+        sliced.sort()
+        answer.append(sliced[k-1])
+        
+    
+    return answer
