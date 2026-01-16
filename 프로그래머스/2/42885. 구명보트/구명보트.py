767630e4a89a78e0d066d4470d3784bd06192714
taeyoung0823 <154206361+taeyoung0823@users.noreply.github.com>
2026-01-16 23:59:24 +0900

    [level 2] Title: 구명보트, Time: 9.87 ms, Memory: 9.58 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -0,0 +1,13 @@
+def solution(people, limit):
+    answer = 0
+    people.sort()
+    i, j = 0, len(people) - 1
+    answer = 0
+
+    while i <= j:
+        if people[i] + people[j] <= limit:
+            i += 1
+        j -= 1
+        answer += 1
+
+    return answer
