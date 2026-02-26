65af8eb93e00380d55cfd814b67fe5198e39857a
taeyoung0823 <154206361+taeyoung0823@users.noreply.github.com>
2026-02-26 14:15:15 +0900

    [level 1] Title: 모의고사, Time: 3.23 ms, Memory: 11.1 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,26 +1,31 @@
 def solution(answers):
     answer = []
-    cnt1=0
-    cnt2=0
-    cnt3=0
-    lst1=[1,2,3,4,5]*len(answers)
-    lst2=[2,1,2,3,2,4,2,5]*len(answers)
-    lst3=[3,3,1,1,2,2,4,4,5,5]*len(answers)
+    ans_1=[1,2,3,4,5]*len(answers)
+    ans_2=[2,1,2,3,2,4,2,5]*len(answers)
+    ans_3=[3,3,1,1,2,2,4,4,5,5]*len(answers)
+    
+    max_correct=0
+    cor_1=0
+    cor_2=0
+    cor_3=0
     
     for i in range(len(answers)):
-        if answers[i]==lst1[i]:
-            cnt1+=1
-        if answers[i]==lst2[i]:
-            cnt2+=1
-        if answers[i]==lst3[i]:
-            cnt3+=1
+        if ans_1[i]==answers[i]:
+            cor_1+=1
+        if ans_2[i]==answers[i]:
+            cor_2+=1
+        if ans_3[i]==answers[i]:
+            cor_3+=1
+    max_correct=max(cor_1,cor_2,cor_3)
     
-    if max(cnt1,cnt2,cnt3)==cnt1:
+    if max_correct==cor_1:
         answer.append(1)
-    if max(cnt1,cnt2,cnt3)==cnt2:
+    if max_correct==cor_2:
         answer.append(2)
-    if max(cnt1,cnt2,cnt3)==cnt3:
+    if max_correct==cor_3:
         answer.append(3)
-        
+    
+    
+    
     
     return answer
