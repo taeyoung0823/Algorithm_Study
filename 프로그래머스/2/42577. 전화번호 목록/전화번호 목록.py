79bb0825b34cc59f444c8ec96c83c0611378ac1e
taeyoung0823 <154206361+taeyoung0823@users.noreply.github.com>
2026-04-14 13:05:42 +0900

    [level 2] Title: 전화번호 목록, Time: 101.07 ms, Memory: 30 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,8 +1,7 @@
 def solution(phone_book):
-    phone_book.sort(key=len)
-    s = set(phone_book)
-    for num in phone_book:
-        for k in range(1, len(num)):
-            if num[:k] in s:
-                return False
+    phone_book.sort()
+
+    for i in range(len(phone_book) - 1):
+        if phone_book[i+1].startswith(phone_book[i]):
+            return False
     return True
