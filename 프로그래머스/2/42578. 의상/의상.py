4ec132c6fd71ec69870d5b071b8049181e6f59df
taeyoung0823 <154206361+taeyoung0823@users.noreply.github.com>
2026-04-16 16:25:35 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,11 +1,14 @@
 def solution(clothes):
-    counts = {}  
-
-    for name, kind in clothes:
-        counts[kind] = counts.get(kind, 0) + 1
-
     answer = 1
-    for c in counts.values():
-        answer *= (c + 1)
-
+    clothes_dict = {}
+    
+    for item, category in clothes:
+        if category not in clothes_dict:
+            clothes_dict[category] = []
+        
+        clothes_dict[category].append(item)
+    
+    for category in clothes_dict:
+        answer *= (len(clothes_dict[category]) + 1)  
+    
     return answer - 1
